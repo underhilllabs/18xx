71cc0e4e81284fa43aaeb7c1063bc7c2144d7172
Bart Lantz <bartlantz@gmail.com>
2021-03-15 16:41:00 -0600

    updates phases with min and maj train limits

--- lib/engine/game/g_18_new_england/game.rb
+++ lib/engine/game/g_18_new_england/game.rb
@@ -159,48 +159,45 @@ module Engine
         PHASES = [
           {
             name: '2',
-            train_limit: '4',
+            train_limit: { minor: 2 },
             tiles: %i[yellow],
             operating_rounds: 2,
           },
           {
             name: '3',
             on: '3',
-            train_limit: 4,
+            train_limit: { minor: 2, major: 4 },
             tiles: %i[yellow green],
             operating_rounds: 2,
           },
           {
             name: '4',
             on: '4',
-            train_limit: 3,
+            train_limit: { minor: 1, major: 3 },
             tiles: %i[yellow green],
             operating_rounds: 2,
           },
           {
             name: '5',
             on: '5E',
-            train_limit: 3,
+            train_limit: { minor: 1, major: 3 },
             tiles: %i[yellow green brown],
             operating_rounds: 2,
           },
           {
             name: '6',
             on: '6E',
-            train_limit: 2,
+            train_limit: { minor: 1, major: 2 },
             tiles: %i[yellow green brown],
             operating_rounds: 2,
           },
           {
             name: '8',
             on: '8E',
-            train_limit: 2,
+            train_limit: { minor: 1, major: 2 },
             tiles: %i[yellow green brown gray],
             operating_rounds: 2,
           },
-          #    status: ['can_buy_companies'],
-          #    status: %w[can_buy_companies export_train],
-          #    status: %w[can_buy_companies export_train],
         ].freeze
 
         TRAINS = [
