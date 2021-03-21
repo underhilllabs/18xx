7d2fdf80549cb41a955241d00cd7fce5d37ba1fa
Bart Lantz <bartlantz@gmail.com>
2021-03-21 15:39:10 -0600

    adds buy company needed for company to bought in phase 1

---
+++ lib/engine/game/g_18_neb/step/buy_company.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative '../../../step/buy_company'
+
+module Engine
+  module Game
+    module G18Neb
+      module Step
+        class BuyCompany < Engine::Step::BuyCompany
+          def can_buy_company?(entity)
+            return true if super
+            return false if entity != current_entity ||
+                            entity.companies.any? ||
+                            !@game.allowed_to_buy_during_operation_round_one?
+
+            companies = @game.purchasable_companies
+            companies.any? && companies.map(&:value).min <= entity.cash
+          end
+        end
+      end
+    end
+  end
+end
+
